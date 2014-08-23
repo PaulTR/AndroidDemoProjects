4330045dc2f8187127cb3c839c770e1c01b5cdf7
Paul Ruiz <paultr@sportslabs.com>
2014-08-22 21:39:58 -0600

    Added Picasso Color Transformation Example

---
+++ IconColorization/app/src/main/java/com/ptrprograms/iconcolorization/activity/MainActivity.java
@@ -0,0 +1,62 @@
+package com.ptrprograms.iconcolorization.activity;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import android.widget.ImageView;
+
+import com.ptrprograms.iconcolorization.R;
+import com.ptrprograms.iconcolorization.utility.ColorTransformation;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private ImageView mDrawableTransformedImage;
+    private ImageView mDrawableUrlTransformedImage;
+    private ImageView mDrawableUrlImage;
+
+    private Target ActionBarIconTarget = new Target()
+    {
+        @Override
+        public void onBitmapLoaded( Bitmap bitmap, Picasso.LoadedFrom from )
+        {
+            getSupportActionBar().setIcon( new BitmapDrawable( getResources(), bitmap ) );
+        }
+
+        @Override
+        public void onBitmapFailed( Drawable errorDrawable )
+        {
+            getSupportActionBar().setIcon( R.drawable.ic_launcher );
+        }
+
+        @Override
+        public void onPrepareLoad( Drawable placeHolderDrawable )
+        {
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDrawableTransformedImage = (ImageView) findViewById( R.id.transformed_drawable );
+        mDrawableUrlImage = (ImageView) findViewById( R.id.url_drawable );
+        mDrawableUrlTransformedImage = (ImageView) findViewById( R.id.transformed_url_drawable );
+
+        Picasso.with( this ).load( R.drawable.ic_star ).transform( new ColorTransformation(getResources().getColor( R.color.local_drawable_color ) ) ).into( mDrawableTransformedImage );
+        Picasso.with( this ).load( getString( R.string.image_url ) ).into( mDrawableUrlImage );
+        Picasso.with( this ).load( getString( R.string.image_url ) ).transform( new ColorTransformation( getResources().getColor( R.color.remote_image_color ) ) ).into( mDrawableUrlTransformedImage );
+
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB )
+            Picasso.with( this ).load( R.drawable.ic_star ).transform( new ColorTransformation( getResources().getColor( R.color.action_bar_icon_color ) ) ).into(ActionBarIconTarget);
+    }
+
+}
---
+++ IconColorization/app/src/main/java/com/ptrprograms/iconcolorization/utility/ColorTransformation.java
@@ -0,0 +1,60 @@
+package com.ptrprograms.iconcolorization.utility;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.BitmapDrawable;
+import com.squareup.picasso.Transformation;
+
+/**
+ * Created by paulruiz on 8/22/14.
+ */
+public class ColorTransformation implements Transformation {
+
+    private int color = 0;
+
+    public ColorTransformation( int color ) {
+        setColor( color );
+    }
+
+    public void setColor( int color ) {
+        this.color = color;
+    }
+
+    public void setColorFromRes( Context context, int colorResId ) {
+        setColor( context.getResources().getColor( colorResId ) );
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    @Override
+    public Bitmap transform(Bitmap source) {
+        if( color == 0 ) {
+            return source;
+        }
+
+        BitmapDrawable drawable = new BitmapDrawable(Resources.getSystem(), source );
+        Bitmap result = Bitmap.createBitmap( drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888 );
+        Canvas canvas = new Canvas( result );
+        drawable.setBounds( 0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight() );
+        drawable.setColorFilter( color, PorterDuff.Mode.SRC_IN );
+        drawable.draw(canvas);
+        drawable.setColorFilter(null);
+        drawable.setCallback(null);
+
+        if( result != source ) {
+            source.recycle();
+        }
+
+        return result;
+    }
+
+    @Override
+    public String key() {
+        return "DrawableColor:" + color;
+    }
+}
