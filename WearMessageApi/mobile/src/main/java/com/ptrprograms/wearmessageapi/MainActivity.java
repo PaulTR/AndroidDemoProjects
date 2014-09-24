f7254f8d5456bf0f3507e5dc42e7e0b110ebaca9
Paul Ruiz <paultr@sportslabs.com>
2014-09-24 16:03:52 -0600

    Adding working code for WearMessageApi

---
+++ WearMessageApi/mobile/src/main/java/com/ptrprograms/wearmessageapi/MainActivity.java
@@ -0,0 +1,106 @@
+package com.ptrprograms.wearmessageapi;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+
+public class MainActivity extends Activity {
+
+    private GoogleApiClient mApiClient;
+
+    private ArrayAdapter<String> mAdapter;
+
+    private ListView mListView;
+    private EditText mEditText;
+    private Button mSendButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+        initGoogleApiClient();
+    }
+
+    private void initGoogleApiClient() {
+        mApiClient = new GoogleApiClient.Builder( this )
+                .addApi( Wearable.API )
+                .build();
+
+        mApiClient.connect();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mApiClient.disconnect();
+    }
+
+    private void init() {
+        mListView = (ListView) findViewById( R.id.list_view );
+        mEditText = (EditText) findViewById( R.id.input );
+        mSendButton = (Button) findViewById( R.id.btn_send );
+
+        mAdapter = new ArrayAdapter<String>( this, android.R.layout.simple_list_item_1 );
+        mListView.setAdapter( mAdapter );
+
+        mSendButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = mEditText.getText().toString();
+                if( !TextUtils.isEmpty( text ) ) {
+                    mAdapter.add( text );
+                    mAdapter.notifyDataSetChanged();
+
+                    sendMessage( text );
+                }
+            }
+        });
+    }
+
+    private void sendMessage( final String text ) {
+        new Thread( new Runnable() {
+            @Override
+            public void run() {
+                NodeApi.GetConnectedNodesResult nodes = Wearable.NodeApi.getConnectedNodes( mApiClient ).await();
+                for(Node node : nodes.getNodes()) {
+                    Log.e("MainActivity", "node id: " + node.getId() );
+                    MessageApi.SendMessageResult result = Wearable.MessageApi.sendMessage(
+                            mApiClient, node.getId(), "/message", text.getBytes() ).await();
+                    if( result.getStatus().isSuccess() ) {
+                        Log.e( "MainActivity", "Message sent successfully" );
+                    }
+
+
+                }
+
+                runOnUiThread( new Runnable() {
+                    @Override
+                    public void run() {
+                        mEditText.setText( "" );
+                    }
+                });
+            }
+        }).start();
+    }
+}
---
+++ WearMessageApi/wear/src/main/java/com/ptrprograms/wearmessageapi/MainActivity.java
@@ -0,0 +1,98 @@
+package com.ptrprograms.wearmessageapi;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Wearable;
+
+public class MainActivity extends Activity implements MessageApi.MessageListener, GoogleApiClient.ConnectionCallbacks {
+
+    private GoogleApiClient mApiClient;
+    private ArrayAdapter<String> mAdapter;
+
+    private ListView mListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mListView = (ListView) findViewById(R.id.list);
+
+        mAdapter = new ArrayAdapter<String>( this, R.layout.list_item );
+        mListView.setAdapter( mAdapter );
+
+        initGoogleApiClient();
+    }
+
+    private void initGoogleApiClient() {
+        mApiClient = new GoogleApiClient.Builder( this )
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .build();
+
+        if( mApiClient != null && !( mApiClient.isConnected() || mApiClient.isConnecting() ) )
+            mApiClient.connect();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if( mApiClient != null && !( mApiClient.isConnected() || mApiClient.isConnecting() ) )
+            mApiClient.connect();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onMessageReceived( final MessageEvent messageEvent) {
+        runOnUiThread( new Runnable() {
+            @Override
+            public void run() {
+                mAdapter.add( new String( messageEvent.getData() ) );
+                mAdapter.notifyDataSetChanged();
+                Toast.makeText( getApplicationContext(), new String( messageEvent.getData() ), Toast.LENGTH_SHORT ).show();
+            }
+        });
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Toast.makeText( this, "Wear onConnected", Toast.LENGTH_SHORT ).show();
+
+        Wearable.MessageApi.addListener( mApiClient, this );
+    }
+
+    @Override
+    protected void onStop() {
+        if ( mApiClient != null ) {
+            Wearable.MessageApi.removeListener( mApiClient, this );
+            if ( mApiClient.isConnected() ) {
+                mApiClient.disconnect();
+            }
+        }
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if( mApiClient != null )
+            mApiClient.unregisterConnectionCallbacks( this );
+        super.onDestroy();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+}
